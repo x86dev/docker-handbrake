df269423c4e3d9a9e7cdcc3578179d20905f3a1c
x86dev <x86dev@users.noreply.github.com>
2019-09-09 21:19:18 +0200

    Ignore partly uploaded files (*.part).

--- watchdog.sh
+++ watchdog.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Watchdog running"
-watchmedo shell-command --patterns="*.mp4;*.avi" --ignore-pattern="*_transcoded*" --recursive --wait --command='transcode.sh "${watch_src_path}"' /mnt/watch
+watchmedo shell-command --patterns="*.mp4;*.avi" --ignore-pattern="*_transcoded*;*.part" --recursive --wait --command='transcode.sh "${watch_src_path}"' /mnt/watch
