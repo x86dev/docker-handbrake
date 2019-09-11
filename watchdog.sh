4a58c31e6a035bf15279b8867c392e1565b3c824
x86dev <x86dev@users.noreply.github.com>
2019-09-11 23:45:43 +0200

    Added run-once.py to Dockerfile and execute it on container start.

--- watchdog.sh
+++ watchdog.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+MY_PATH_WATCH=/mnt/watch
+
+echo "Running once ..."
+run-once ${MY_PATH_WATCH}
+
 echo "Watchdog running"
-watchmedo shell-command --patterns="*.mp4;*.avi" --ignore-pattern="*_transcoded*;*.part" --recursive --wait --command='transcode.sh "${watch_src_path}"' /mnt/watch
+watchmedo shell-command --patterns="*.mp4;*.avi" --ignore-pattern="*_transcoded*;*.part" --recursive --wait --command='transcode.sh "${watch_src_path}"' ${MY_PATH_WATCH}
