d3d321311b77534544ee724c61052bd67d8970a8
x86dev <x86dev@users.noreply.github.com>
2019-09-11 23:54:42 +0200

    Fixed calling run-once.py.

--- watchdog.sh
+++ watchdog.sh
@@ -5,7 +5,7 @@ MY_PATH_WATCH=/mnt/watch
 echo "Path is: $MY_PATH_WATCH"
 
 echo "Running once ..."
-run-once ${MY_PATH_WATCH}
+run-once.py ${MY_PATH_WATCH}
 
 echo "Watchdog running"
 watchmedo shell-command --patterns="*.mp4;*.avi" --ignore-pattern="*_transcoded*;*.part" --recursive --wait --command='transcode.sh "${watch_src_path}"' ${MY_PATH_WATCH}
