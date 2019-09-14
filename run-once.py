10f554685b8f043a22778d9169dfb370527b75fb
x86dev <x86dev@users.noreply.github.com>
2019-09-14 16:41:51 +0200

    run-once.py: Added skipping certain file suffixes, cleaned up and improved syntax.

--- run-once.py
+++ run-once.py
@@ -7,14 +7,27 @@ import sys
 import subprocess
 
 if len(sys.argv) >= 2:
-    path = sys.argv[1]
+    strPath = sys.argv[1]
 else:
-    path = '.'
+    strPath = '.'
 
-print('Handling path: %s' % path)
+g_fVerbose            = False
+g_aFileExtToTranscode = ('.mp4', '.avi', '.mpg', '.wmv', '.mkv')
+g_aFileSuffixesToSkip = ("_original", "_transcoded")
 
-for (dirpath, dirnames, filenames) in walk(path):
-    for fname in filenames:
-        if fname.lower().endswith(('.mp4', '.avi', '.mpg', '.wmv', '.mkv')):
-            fname_abs = dirpath + '/' + fname
-            subprocess.call(['transcode.sh', fname_abs])
+print('Handling path: %s' % strPath)
+
+for (strDirPath, aDirNames, aFiles) in walk(strPath):
+    for strFileName in aFiles:
+        fHandled = False
+        strFilePathAbs = strDirPath + '/' + strFileName
+        if strFilePathAbs.lower().endswith(g_aFileExtToTranscode):
+            strFileNameNoExt = os.path.splitext(strFilePathAbs)[0]
+            if not strFileNameNoExt.lower().endswith(g_aFileSuffixesToSkip):
+                print('Handling: %s' % strFilePathAbs)
+                subprocess.call(['transcode.sh', strFilePathAbs])
+                fHandled = True
+
+        if not fHandled \
+           and g_fVerbose:
+            print('Skipping: %s' % strFilePathAbs)
