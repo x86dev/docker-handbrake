f644668571ef95638e5c24e938d1ea7844004ac7
x86dev <x86dev@users.noreply.github.com>
2019-10-06 21:20:24 +0200

    watchdog.sh: Logging.

--- watchdog.sh
+++ watchdog.sh
@@ -7,5 +7,11 @@ echo "Path is: $MY_PATH_WATCH"
 echo "Running once ..."
 handbrake_transcode_dir.py ${MY_PATH_WATCH}
 
-echo "Watchdog running"
-watchmedo shell-command --patterns="*.mp4;*.avi" --ignore-pattern="*_transcoded*;*_original*;*.part" --recursive --wait --command='transcode.sh "${watch_src_path}"' ${MY_PATH_WATCH}
+MY_PATTERN_INCLUDE="*.mp4;*.avi"
+MY_PATTERN_IGNORE="*_transcoded*;*_original*;*.part"
+
+echo "Watchdog running ..."
+echo "Include pattern: $MY_PATTERN_INCLUDE"
+echo "Ignore pattern: $MY_PATTERN_IGNORE"
+
+watchmedo shell-command --patterns="$MY_PATTERN_INCLUDE" --ignore-pattern="$MY_PATTERN_IGNORE" --recursive --wait --command='transcode.sh "${watch_src_path}"' ${MY_PATH_WATCH}
