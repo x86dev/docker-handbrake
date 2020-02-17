60e0a9a7c505678a8867e04731f0af48c3adf940
x86dev <x86dev@users.noreply.github.com>
2020-02-17 10:53:12 +0100

    transcode.sh: Throttle in case the watchdog crashed.

--- watchdog.sh
+++ watchdog.sh
@@ -2,16 +2,24 @@
 
 MY_PATH_WATCH=/mnt/watch
 
+MY_PATTERN_INCLUDE="*.mp4;*.avi"
+MY_PATTERN_IGNORE="*_transcoded*;*_original*;*.part"
+
 echo "Path is: $MY_PATH_WATCH"
+echo "Include pattern: $MY_PATTERN_INCLUDE"
+echo "Ignore pattern: $MY_PATTERN_IGNORE"
 
 echo "Running once ..."
 handbrake_transcode_dir.py "$MY_PATH_WATCH"
 
-MY_PATTERN_INCLUDE="*.mp4;*.avi"
-MY_PATTERN_IGNORE="*_transcoded*;*_original*;*.part"
+# Raise inode limt. Needs root.
+sysctl fs.inotify.max_user_watches=65536
 
 echo "Watchdog running ..."
-echo "Include pattern: $MY_PATTERN_INCLUDE"
-echo "Ignore pattern: $MY_PATTERN_IGNORE"
 
 watchmedo shell-command --patterns="$MY_PATTERN_INCLUDE" --ignore-pattern="$MY_PATTERN_IGNORE" --recursive --wait --command='transcode.sh "${watch_src_path}"' ${MY_PATH_WATCH}
+
+echo "Watchdog terminated"
+
+# Don't restart too fast in case that the watchdog crashed.
+sleep 30
