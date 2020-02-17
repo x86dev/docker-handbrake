ef79f50620f7bb6995cdea54bef9ba7ffc1a1136
x86dev <x86dev@users.noreply.github.com>
2020-02-17 12:36:27 +0100

    transcode.sh: Install a signal handler and a separate cleanup routine to have it all in a centralized place.

--- transcode.sh
+++ transcode.sh
@@ -28,14 +28,41 @@ MY_FILENAME_DST=${MY_FILENAME_PATH}/${MY_FILENAME_DST_NAME}
 MY_FILENAME_DST_OLD_SCHEME=${MY_FILENAME_PATH}/${MY_FILENAME_PREFIX_TRANSCODED}${MY_FILENAME_NAME}
 MY_FILENAME_LOG=${MY_FILENAME_DST}.log
 
+MY_ERROR=
+
+cleanup() 
+{
+    echo "Cleaning up ..."
+
+    if [ -n "$MY_ERROR" ]; then
+        echo "An error ocurred -- see '$MY_FILENAME_LOG'"
+        # Apply the source file's access rights to the log file,
+        # so that the user can delete it with the same rights later.
+        chown $(stat -c '%U.%G' "$MY_FILENAME_SRC") "$MY_FILENAME_LOG"
+        # Delete partially encoded files again.
+        rm "$MY_FILENAME_DST" > /dev/null 2>&1
+        rm "$MY_FILENAME_DST_TMP" > /dev/null 2>&1
+    else
+        # Remove the log file on success.
+        rm "$MY_FILENAME_LOG" > /dev/null 2>&1
+    fi
+}
+
+# Install signal handling.
+trap cleanup INT TERM
+
 log()
 {
     echo "[$(date)] $1" | tee -a "$MY_FILENAME_LOG" > /dev/null 2>&1
 }
 
-log "Handling '$MY_FILENAME'"
+error()
+{
+    echo "[$(date)] ERROR: $1" | tee -a "$MY_FILENAME_LOG" > /dev/null 2>&1
+    MY_ERROR=1
+}
 
-MY_ERROR=
+log "Handling '$MY_FILENAME'"
 
 mkdir -p "$MY_PATH_TMP"
 if [ $? -ne 0 ]; then
@@ -45,7 +72,7 @@ fi
 
 MY_SRC_CODEC_TYPE=$(ffprobe -v error -hide_banner -of default=noprint_wrappers=1:nokey=1 -select_streams v:0 -show_entries stream=codec_name "$MY_FILENAME_SRC")
 if [ $? -ne 0 ]; then
-    log "Unable to determine codec type, skipping: $MY_FILENAME_SRC"
+    error "Unable to determine codec type, skipping: $MY_FILENAME_SRC"
     exit 1
 fi
 if [ "$MY_SRC_CODEC_TYPE" == "hevc" ]; then
@@ -54,24 +81,22 @@ if [ "$MY_SRC_CODEC_TYPE" == "hevc" ]; then
 fi
 if [ -f "$MY_FILENAME_DST" ]; then
     log "Destination file already exists, skipping: $MY_FILENAME_SRC"
-    exit 1
+    exit 0
 fi
 if [ -f "$MY_FILENAME_DST_OLD_SCHEME" ]; then
     log "Destination file (old scheme) already exists, skipping: $MY_FILENAME_SRC"
-    exit 1
+    exit 0
 fi
 log "Transcoding started: $MY_FILENAME_SRC ($MY_SRC_CODEC_TYPE) -> $MY_FILENAME_DST_TMP"
 HandBrakeCLI --preset-import-file "$MY_PROFILE_FILE" -i "$MY_FILENAME_SRC" -o "$MY_FILENAME_DST_TMP" --preset="$MY_PROFILE_NAME" 2>&1 | tee -a "$MY_FILENAME_LOG" > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    log "Error: Could not transcode file: $MY_FILENAME_SRC"
-    MY_ERROR=1
+    error "Could not transcode file: $MY_FILENAME_SRC"
 else
     log "Transcoding successful: $MY_FILENAME_SRC"
     log "Moving temp file '$MY_FILENAME_DST_TMP' to '$MY_FILENAME_DST' ..."
     mv "$MY_FILENAME_DST_TMP" "$MY_FILENAME_DST" > /dev/null 2>&1
     if [ $? -ne 0 ]; then
-        log "Error: Moving temp file '$MY_FILENAME_DST_TMP' to '$MY_FILENAME_DST' failed"
-        MY_ERROR=1
+        error "Moving temp file '$MY_FILENAME_DST_TMP' to '$MY_FILENAME_DST' failed"
     else
         log "Cloning file attributes ..."
         chown $(stat -c '%U.%G' "$MY_FILENAME_SRC") "$MY_FILENAME_DST" | tee -a "$MY_FILENAME_LOG" > /dev/null 2>&1
@@ -88,26 +113,13 @@ else
                 mv "$MY_FILENAME_SRC" "$MY_FILENAME_PATH/$MY_FILENAME_NAME_NO_EXT$MY_FILENAME_SUFFIX_ORIGINAL.$MY_FILENAME_EXT" \
                 && mv "$MY_FILENAME_DST" "$MY_FILENAME_SRC"
                 if [ $? -ne 0 ]; then
-                    log "Error: Replacing file failed: $MY_FILENAME_SRC"
-                    MY_ERROR=1
+                    error "Replacing file failed: $MY_FILENAME_SRC"                    
                 fi
             fi
         else
-            log "Error: Destination duration is less than 90% of the source duration"
-            MY_ERROR=1
+            error "Destination duration is less than 90% of the source duration"
         fi
     fi
 fi
 
-if [ -n "$MY_ERROR" ]; then
-    log "An error ocurred -- see '$MY_FILENAME_LOG'"
-    # Keep the log file, but apply the file's access rights to it,
-    # so that the user can delete it with the same rights later.
-    chown $(stat -c '%U.%G' "$MY_FILENAME_SRC") "$MY_FILENAME_LOG"
-    # Delete partially encoded files again.
-    rm "$MY_FILENAME_DST" > /dev/null 2>&1
-    rm "$MY_FILENAME_DST_TMP" > /dev/null 2>&1
-else
-    # Remove the log file on success.
-    rm "$MY_FILENAME_LOG"
-fi
+cleanup
